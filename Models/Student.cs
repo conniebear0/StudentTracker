79916e2f710dedfb6450d8e98a530a8b5037b091
ConnieM <moppconnavar@gmaol.com>
2026-08-10 16:36:23 +0200

    Updated Student.cs file to properly contain properties StudentID, First and Last name, Gneder and Faculty

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,6 +1,17 @@
-﻿namespace StudentTracker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentTracker.Models
 {
     public class Student
     {
+        public int StudentID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Faculty { get; set; }
     }
 }
