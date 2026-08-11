1affa4a6ba9f1cfd56041294362815816beab340
ConnieM <moppconnavar@gmaol.com>
2026-08-11 10:45:09 +0200

    Added the services folder with the dbcontext class for the student model

---
+++ Services/StudentDbContext.cs
@@ -0,0 +1,14 @@
+﻿using StudentTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentTracker.Services
+{
+    public class StudentDbContext: DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+
+}
