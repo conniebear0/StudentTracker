e913f05e07b76420dd904b24c77d4644193b8e4b
ConnieM <moppconnavar@gmaol.com>
2026-08-11 11:19:03 +0200

    Added the dbContext service to the main program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using StudentTracker.Services;
+
 namespace StudentTracker
 {
     public class Program
@@ -8,6 +11,8 @@ namespace StudentTracker
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<StudentDbContext>(options => options.UseInMemoryDatabase("StudentDb"));
 
             var app = builder.Build();
 
